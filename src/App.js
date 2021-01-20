25de5e067e39577b6c61ce0bc1d516b9976b4627
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 14:30:46 -0600

    Added fetch procedure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
   const [data, setData] = useState([]);
   const [keyword, setKeyword] = useState('');
   const fetchData = () => {
-    // REST API call comes here
+    const url = `https://api.github.com/search/repositories?q=${keyword}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(responseData => {
+        setData(responseData.items);
+      });
   }
   const handleChange = (e) => {
     setKeyword(e.target.value);

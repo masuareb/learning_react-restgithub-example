18bb25b59039dbc8282452b6dd0e4ea936e5fb59
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 14:55:25 -0600

    Remove of html table for use of react-table

--- src/App.js
+++ src/App.js
@@ -15,15 +15,10 @@ function App() {
   const handleChange = (e) => {
     setKeyword(e.target.value);
   }
-  const tableRows = data.map((item, index) =>
-    <tr key={index}><td>{item.full_name}</td>
-    <td><a href={item.html_url}>{item.html_url}</a></td></tr>
-  );
   return (
     <div className="App">
       <input type="text" onChange={handleChange} />
       <button onClick={fetchData} value={keyword}>fetch</button>
-      <table><tbody>{tableRows}</tbody></table>
     </div>
   );
 }

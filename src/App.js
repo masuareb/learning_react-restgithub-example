a08b7d48c7b174701858853e0c377ed821f35075
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 13:41:07 -0600

    Added state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [keyword, setKeyword] = useState('');
+  const fetchData = () => {
+    // REST API call comes here
+  }
   return (
     <div className="App">
     </div>
